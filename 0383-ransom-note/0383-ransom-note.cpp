cdc310ece831bf986c836ee3f5ebdc5129d3a48a
Vaibhav Rai <dhruv2005rai@gmail.com>
2026-09-22 22:47:58 +0530

    Time: 0 ms (100%), Space: 11.9 MB (28.88%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq_1(26,0);
+        vector<int> freq_2(26,0);
+        int m=ransomNote.size();
+        int n=magazine.size();
+        for(int i=0;i<m;i++){
+            freq_1[ransomNote[i]-'a']++;
+        }
+        for(int i=0;i<n;i++){
+            freq_2[magazine[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq_1[i]>freq_2[i]){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
