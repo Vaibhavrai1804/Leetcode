e7c159a5bc9916f6db639c46aa6f27a7b7c7886f
Vaibhav Rai <dhruv2005rai@gmail.com>
2026-09-24 17:20:00 +0530

    Time: 1 ms (52.23%), Space: 9 MB (98.91%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        else{
+            for(int i=0;i<s.size();i++){
+                if(s.find(s[i])!=t.find(t[i])){
+                    return false;
+                    
+                }
+            }
+            return true ;
+        }
+        
+    }
+};
